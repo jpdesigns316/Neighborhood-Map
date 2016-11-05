be2a6c7e7a2854723f707210c313423297e86429
Jon Peterson <jonathandpeterson@yahoo.com>
2016-11-04 22:18:23 -0700

    Add search button and made search box slideToggle

--- js/app.js
+++ js/app.js
@@ -1,57 +1,57 @@
 'use strict';
 
-var initialLocations = [
-	{name: 'Arizona Charlie\'s Boulder', lat: 36.1241978, long: -115.0758045 },
-  {name: 'Boulder Station', lat: 36.13269199999999, long: -115.084768},
-  {name: 'Club Fortune', lat: 36.017758, long: -114.948167},
-  {name: 'Eldorado Casino',  lat: 36.032018, long: -114.983564},
-  {name: 'Emerald Island Casino', lat: 36.032655, long: -114.984652},
-  {name: 'Joker\'s Wild', lat: 36.051963, long: -114.994669},
-  {name: 'Kahootz Bar', lat: 36.130155, long: -115.082661},
-  {name: 'Lake Mead Casino', lat: 36.052813, long: -114.968051},
-  {name: 'Longhorn Casino', lat: 36.107087, long: -115.059146},
-  {name: 'My Casino', lat: 36.024121, long: -114.962872},
-  {name: 'Railroad Pass Hotel & Casino', lat: 35.972519, long: -114.91197},
-  {name: 'Rainbow Club', lat: 36.032702, long: -114.983688},
-  {name: 'Skyline Restaurant and Casino', lat: 36.062479 , long: -115.008526},
-  {name: 'Sunset Station Hotel & Casino', lat: 35.981265 , long: -114.916838},
-  {name: 'Terrible\s Town Casino & Bowl', lat: 35.981265 , long: -114.916838},
-  {name: 'Dotty\'s #62 & #80', lat: 36.130324, long: -115.082407},
-  {name: 'Dotty\'s #76', lat: 36.030153, long: -114.971208},
-  {name: 'Dotty\'s #8', lat: 36.050587, long: -114.994591}
-];
-
 // Declaring global variables now to satisfy strict mode
 var map;
 var clientID;
 var clientSecret;
 
+// Foursquare API settings
+clientID = "TP2NQO5LEORRMRP52IGDGN2MGOJKTBV4A3VNVOGAV5JMG23M";
+clientSecret = "1AEYC4PEHVPBR12QRVTXHVOSSHASE4YLM1FMNELLFSG10CVJ";
+
+
+var initialLocations = [
+	{name: 'Calvary Chapel Vista', lat: 33.2106, long: -117.2333},
+	{name: 'Calvary Chapel Carlsbad', lat: 33.1202596, long: -117.27769219999999},
+	{name: 'Chik-Fil-A', lat: 33.1779847, long: -117.29627310000001},
+	{name: 'Miracosta College', lat: 33.1908, long: -117.3029},
+	{name: 'Ocean\'s 11 Casino', lat: 33.1992609, long: -117.36803050000003},
+	{name: 'Pair-A-Dice Games', lat: 33.1841, long: -117.2846},
+	{name: 'Teri Cafe II', lat: 33.1823806, long: -117.29232430000002},
+
+];
+
 
 // formatPhone function referenced from
 // http://snipplr.com/view/65672/10-digit-string-to-phone-format/
 
-function formatPhone(phonenum) {
+function formatPhone(phonenum)
+{
     var regexObj = /^(?:\+?1[-. ]?)?(?:\(?([0-9]{3})\)?[-. ]?)?([0-9]{3})[-. ]?([0-9]{4})$/;
-    if (regexObj.test(phonenum)) {
+    if (regexObj.test(phonenum))
+		{
         var parts = phonenum.match(regexObj);
         var phone = "";
         if (parts[1]) { phone += "+1 (" + parts[1] + ") "; }
         phone += parts[2] + "-" + parts[3];
         return phone;
     }
-    else {
+    else
+		{
         //invalid phone number
         return phonenum;
     }
 }
 
-var Location = function(data) {
+var Location = function(data)
+{
 	var self = this;
 	this.name = data.name;
 	this.lat = data.lat;
 	this.long = data.long;
 	this.URL = "";
 	this.street = "";
+	this.description = "";
 	this.city = "";
 	this.phone = "";
 
@@ -66,55 +66,63 @@ var Location = function(data) {
 			self.URL = "";
 		}
 		self.street = results.location.formattedAddress[0];
-     	self.city = results.location.formattedAddress[1];
-      	self.phone = results.contact.phone;
-      	if (typeof self.phone === 'undefined'){
+   	self.city = results.location.formattedAddress[1];
+		self.phone = results.contact.phone;
+    if (typeof self.phone === 'undefined')
+		{
 			self.phone = "";
-		} else {
+		} else
+		{
 			self.phone = formatPhone(self.phone);
 		}
-	}).fail(function() {
+	}).fail(function()
+	{
 		alert("There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.");
 	});
 
-	this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
-        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+	this.contentString = '<div class="info-window-content"><div><b>' + data.name + "</b></div>" +
+				'<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
         '<div class="content">' + self.street + "</div>" +
         '<div class="content">' + self.city + "</div>" +
         '<div class="content">' + self.phone + "</div></div>";
 
 	this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
 
-	this.marker = new google.maps.Marker({
+	this.marker = new google.maps.Marker(
+		{
 			position: new google.maps.LatLng(data.lat, data.long),
 			map: map,
 			title: data.name
 	});
 
-	this.showMarker = ko.computed(function() {
+	this.showMarker = ko.computed(function()
+	{
 		if(this.visible() === true) {
 			this.marker.setMap(map);
-		} else {
+		} else
+		{
 			this.marker.setMap(null);
 		}
 		return true;
 	}, this);
 
-	this.marker.addListener('click', function(){
-		self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+	this.marker.addListener('click', function()
+	{
+		self.contentString = '<div class="info-window-content"><strong>' + data.name + "</strong></div>" +
         '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
         '<div class="content">' + self.street + "</div>" +
         '<div class="content">' + self.city + "</div>" +
         '<div class="content"><a href="tel:' + self.phone +'">' + self.phone +"</a></div></div>";
 
-        self.infoWindow.setContent(self.contentString);
+    self.infoWindow.setContent(self.contentString);
 
 		self.infoWindow.open(map, this);
 
 		self.marker.setAnimation(google.maps.Animation.BOUNCE);
-      	setTimeout(function() {
-      		self.marker.setAnimation(null);
-     	}, 2100);
+  	setTimeout(function()
+		{
+  		self.marker.setAnimation(null);
+ 		}, 2100);
 	});
 
 	this.bounce = function(place) {
@@ -122,36 +130,40 @@ var Location = function(data) {
 	};
 };
 
-function AppViewModel() {
+function AppViewModel()
+{
 	var self = this;
 
 	this.searchTerm = ko.observable("");
 
 	this.locationList = ko.observableArray([]);
 
-var las_vegas = {lat: 36.082479 , lng: -115.008526};
-	map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'),
+	{
 			zoom: 12,
-			center: {lat: 36.082479 , lng: -115.008526}
+			center: {lat: 33.18247, lng: -117.299738}
 	});
 
-	// Foursquare API settings
-	clientID = "TP2NQO5LEORRMRP52IGDGN2MGOJKTBV4A3VNVOGAV5JMG23M";
-	clientSecret = "1AEYC4PEHVPBR12QRVTXHVOSSHASE4YLM1FMNELLFSG10CVJ";
 
-	initialLocations.forEach(function(locationItem){
+	initialLocations.forEach(function(locationItem)
+	{
 		self.locationList.push( new Location(locationItem));
 	});
 
-	this.filteredList = ko.computed( function() {
+	this.filteredList = ko.computed( function()
+	{
 		var filter = self.searchTerm().toLowerCase();
-		if (!filter) {
-			self.locationList().forEach(function(locationItem){
+		if (!filter)
+		{
+			self.locationList().forEach(function(locationItem)
+			{
 				locationItem.visible(true);
 			});
 			return self.locationList();
-		} else {
-			return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
+		} else
+		{
+			return ko.utils.arrayFilter(self.locationList(), function(locationItem)
+			{
 				var string = locationItem.name.toLowerCase();
 				var result = (string.search(filter) >= 0);
 				locationItem.visible(result);
@@ -164,10 +176,12 @@ var las_vegas = {lat: 36.082479 , lng: -115.008526};
 	this.mapElem.style.height = window.innerHeight - 50;
 }
 
-function startApp() {
+function startApp()
+{
 	ko.applyBindings(new AppViewModel());
 }
 
-function errorHandling() {
+function errorHandling()
+{
 	alert("Google Maps has failed to load. Please check your internet connection and try again.");
 }
