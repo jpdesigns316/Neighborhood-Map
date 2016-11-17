c8964253e61db5fcb63c172790a5ec9d0e062996
Jon Peterson <jonathandpeterson@yahoo.com>
2016-11-16 18:25:48 -0800

    Finished writing the Readme

--- js/app.js
+++ js/app.js
@@ -11,27 +11,45 @@ var CLIENT_SECRET = "1AEYC4PEHVPBR12QRVTXHVOSSHASE4YLM1FMNELLFSG10CVJ";
 var initialLocations = [{
         name: 'Chik-Fil-A',
         lat: 33.1779847,
-        long: -117.29627310000001
+        long: -117.29627310000001,
+        categories: "Fast Food Restaurant",
+        foursquareLink: "chickfila-quarry-creek/4b0d93b1f964a520674b23e3"
     }, {
         name: 'Buffalo Wild Wings',
         lat: 33.18185649999999,
-        long: -117.32919190000001
+        long: -117.32919190000001,
+        categories: "Wings Joint",
+        foursquareLink: "buffalo-wild-wings/53a492c0498ea5554f6182c4"
     }, {
         name: 'Fresh MXN Food',
         lat: 33.138909,
-        long: -117.19882010000003
+        long: -117.19882010000003,
+        categories: "Mexican Restaurant",
+        foursquareLink: "fresh-mxn-food-san-marcos/4abed1eff964a520159020e3"
     }, {
         name: 'KFC',
         lat: 33.2104598,
-        long: -117.23445939999999
+        long: -117.23445939999999,
+        categories: "Fried Chicken Joint",
+        foursquareLink: "kfc/4bb81ad93db7b713480c219a"
+    }, {
+        name: 'Ocean\'s Eleven Casino',
+        lat: 33.2000485,
+        long: -117.36925450000001,
+        categories: "Casino",
+        foursquareLink: "oceans-eleven-casino/4b5b5e04f964a52037f828e3"
     }, {
         name: 'Teri Cafe I',
-        lat: 33.1858874,
-        long: -117.32724159999998
+        lat: 33.18524330989122,
+        long: -117.3272989435073,
+        categories: "Japanese Restaurant",
+        foursquareLink: "teri-cafe/4a8b5a56f964a5203a0c20e3"
     }, {
         name: 'Teri Cafe II',
         lat: 33.1823806,
-        long: -117.29232430000002
+        long: -117.29232430000002,
+        categories: "Japanese Restaurant",
+        id: "teri-cafe/4b0749b1f964a52059fb22e3"
     }
 
 ];
@@ -45,68 +63,41 @@ var Location = function(data) {
     this.name = data.name;
     this.lat = data.lat;
     this.long = data.long;
-    this.URL = '';
-    this.street = '';
-    this.city = '';
-    this.phone = '';
-    this.twitter = '';
-    this.categories = '';
     this.menu = '';
+    this.categories = '';
+
     this.contentString = '';
 
     this.visible = ko.observable(true);
 
 
-    // Check to see if client web browser is a mobile device.
-    // Credit:
-    // http://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
-    self.isMobile = function() {
-        if (navigator.userAgent.match(/Android/i) ||
-            navigator.userAgent.match(/webOS/i) ||
-            navigator.userAgent.match(/iPhone/i) ||
-            navigator.userAgent.match(/iPad/i) ||
-            navigator.userAgent.match(/iPod/i) ||
-            navigator.userAgent.match(/BlackBerry/i) ||
-            navigator.userAgent.match(/Windows Phone/i)
-        ) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+
 
     // Generate FourSqurare JSON to data mine
     var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll=' +
         this.lat + ',' + this.long + '&client_id=' + CLIENT_ID +
-        '&client_secret=' + CLIENT_SECRET + '&v=20160118' +
+        '&client_secret=' + CLIENT_SECRET + '&v=20161114' +
         '&query=' + this.name;
 
     $.getJSON(foursquareURL).done(function(data) {
         var results = data.response.venues[0];
-
-        self.URL = results.url;
-        self.street = results.location.formattedAddress[0];
-        self.city = results.location.formattedAddress[1];
-        self.phone = results.contact.formattedPhone;
         self.twitter = results.contact.twitter;
         self.categories = results.categories[0].name;
 
-        if (!self.mobile) {
+        try {
             self.menu = results.menu.url;
-        } else {
-            self.menu = results.menu.mobileUrl;
+        } catch (err) {
+            self.menu = '';
         }
+
 
         // Logic to not display 'undefined'. Checks to see if string in
         // undefined, if so then leave blank.
-        self.phone = self.phone ? self.phone : "";
-        self.url = self.url ? self.url : "";
-        self.twitter = self.twitter ? self.twitter : "";
 
-        if (self.menu === 'undefined') {
-            self.url = '';
-            self.mobileUrl = '';
-        }
+        self.twitter = self.twitter ? self.twitter : "";
+        self.menu = self.menu ? self.menu : "";
+
+
     }).fail(function() {
         alert("There was an error with the Foursquare API call. Please " +
             "refresh the page and try again to load Foursquare data.");
@@ -118,11 +109,13 @@ var Location = function(data) {
         content: self.contentString
     });
 
+
     this.marker = new google.maps.Marker({
         position: new google.maps.LatLng(data.lat, data.long),
         map: map,
         title: data.name
     });
+
 
     this.showMarker = ko.computed(function() {
         if (this.visible() === true) {
@@ -138,13 +131,14 @@ var Location = function(data) {
         // Add information to the #info-panel for more in-depth infomation
         // about the chosen location.
         self.infoContent = '<div class="infor-panel-content">' +
-            '<div><strong>' + data.name + '</strong></div>' +
-            '<div class="info-content"><a href="' +
-            self.URL + '">' + self.URL + '</a></div>' +
-            '<div class="info-content">' + self.street + '</div>' +
-            '<div class="info-content">' + self.city + '</div>' +
-            '<div class="info-content"><strong>Menu:</strong> <a href="' +
-            self.menu + '" target="_blank">' + self.menu + '</a></div>';
+            '<div><strong><a href="https://foursquare.com/v/' +
+            data.foursquareLink + '" target="_blank">' + data.name +
+            '</a></strong></div>';
+
+        if (self.menu !== '') {
+            self.infoContent += '<div><a href="' + self.menu +
+                '" target="_blank">Click for Menu</a></div>';
+        }
 
         // Add Twitter account if location has one
         if (self.twitter !== '') {
@@ -154,10 +148,8 @@ var Location = function(data) {
                 '</a></div>';
         }
 
-        self.infoContent += '<div class="info-content"><strong>Phone:' +
-            '</strong> <a href="tel:' + self.phone + '">' + self.phone +
-            '</a></div><div class="info-content"><strong>Categories: ' +
-            '</strong>' + self.categories + '</div></div>';
+        self.infoContent += '<div class="info-content"><strong>Categories: ' +
+            '</strong>' + data.categories + '</div></div>';
 
         // Add an informtion to the #info-panel to be displayed there.
 
@@ -178,7 +170,11 @@ var Location = function(data) {
 
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    var mapID = document.getElementById('map');
+    var foursquareLogo = document.createElement("IMG");
+    var searchBar = document.getElementsByClassName('search-bar');
+
+    map = new google.maps.Map(mapID, {
         zoom: 13,
         center: {
             lat: 33.18247,
@@ -188,13 +184,17 @@ function initMap() {
             style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
             position: google.maps.ControlPosition.BOTTOM_CENTER
         },
-        scrollwheel: false
+
     });
 
-    var foursquareLogo = document.createElement("IMG");
+    google.maps.event.trigger(map, "resize");
+
+    // Using Custom Controls to add images to map
+
     foursquareLogo.setAttribute("src", "images/logo_foursquare.png");
     foursquareLogo.setAttribute("alt", "Data provided by Foursqure.com");
 
+    map.controls[google.maps.ControlPosition.TOP].push(searchBar[0]);
     map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(foursquareLogo);
 
 }
